d9989a85634c9e41d3924558f9bca4a44fa48c5f
Ed Harrod <echarrod@users.noreply.github.com>
2025-09-02 15:10:40 +0100

    danger-js: Update dangerfile to use new Git interface methods

--- build/ci/dangerfile.go
+++ build/ci/dangerfile.go
@@ -8,6 +8,6 @@ import (
 
 // Run is invoked by danger-go
 func Run(d *danger.T, pr danger.DSL) {
-	d.Message(fmt.Sprintf("%d new files added!", len(pr.Git.CreateFiles)), "", 0)
-	d.Message(fmt.Sprintf("%d files modified!", len(pr.Git.ModifiedFiles)), "", 0)
+	d.Message(fmt.Sprintf("%d new files added!", len(pr.Git.GetCreatedFiles())), "", 0)
+	d.Message(fmt.Sprintf("%d files modified!", len(pr.Git.GetModifiedFiles())), "", 0)
 }
